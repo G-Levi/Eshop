925f59f6b0d4592a9b6ce840333881d54d25e07c
G.Levi <guczuilevente@gmail.com>
2018-07-24 09:20:34 +0300

    Eshop_V0.1.8_G.Levi_Controllers_CustomerService.cs

---
+++ Eshop/Eshop/Controllers/CustomerServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class CustomerServiceController : Controller
+    {
+        // GET: CustomerService
+        public ActionResult CustomerService()
+        {
+            return View();
+        }
+    }
+}
