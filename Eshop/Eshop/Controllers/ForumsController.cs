2c43ab90127b29ed8bb7099967708c9dc880401d
Levente Guczui <guczuilevente@outlook.com>
2018-07-24 10:52:41 +0300

    Eshop_V0.1.9_G.Levi_Controllers_ForumsController.cs

---
+++ Eshop/Eshop/Controllers/ForumsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class ForumsController : Controller
+    {
+        // GET: Forums
+        public ActionResult Forums()
+        {
+            return View();
+        }
+    }
+}
