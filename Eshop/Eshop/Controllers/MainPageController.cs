ce65e9e5aea0a2b65a8ef6b83f4475acc0b27dbc
G.Levi <guczuilevente@gmail.com>
2018-07-25 20:19:37 +0300

    Eshop_V0.3.6_G.Levi_Controllers_MainPageController.cs

---
+++ Eshop/Eshop/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
